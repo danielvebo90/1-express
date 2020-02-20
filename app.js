28a479909876a76e822eeb08c73a7ff70e8f7fda
danielvebo90 <danielvebo@gmail.com>
2020-02-19 19:52:13 -0500

    3 repo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hola " + req.query.name);
+  res.send("<h1> Hola " + req.query.name +"! </h1>");
 });
 
 
